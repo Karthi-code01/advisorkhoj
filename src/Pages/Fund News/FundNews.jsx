d0c4d796620c24280c6655aa819047cf139e3ff1
karthi <karthiuced@gmail.com>
2025-09-15 18:52:06 +0530

    done

--- src/Pages/Fund News/FundNews.jsx
+++ src/Pages/Fund News/FundNews.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import Header from '../Header'
 import Breadcrumbs from '../Breadcrumbs'
-import { Search } from 'lucide-react'
+import { ArrowRight, Search } from 'lucide-react'
 import Footer from '../Footer'
 import FundNewsPagination from './FundNewsPagination'
+import aimImage from "../../assets/Images/6418fd258e033e4afc684202f0875f873e862988.png";
+import image3 from '../../assets/Images/3153823c1f81f2fe586e4b9dae485b114e674bd4.png'
+import image4 from '../../assets/Images/a704b8a182e131059a12b4ffc0d55fffe2bb89fc.png'
+import tool9 from "../../assets/Images/Union (2).png"
+import tool10 from "../../assets/Images/Union (3).png"
+import tool11 from "../../assets/Images/Union (4).png"
+import tool12 from "../../assets/Images/advisory tools (6).png"
+import tool13 from "../../assets/Images/advisory tools (7).png"
+import tool14 from "../../assets/Images/advisory tools (8).png"
+import tool15 from "../../assets/Images/advisory tools (9).png"
+import tool16 from "../../assets/Images/advisory tools (10).png"
 
 function FundNews() {
-  return (
-     <div className="min-h-screen bg-[#f8f9f9]">
+    const features2 = [
+        { id: 1, name: "Crorepati Calculator", icon: tool9 },
+        { id: 2, name: "Risk Analyser", icon: tool10 },
+        { id: 3, name: "SIP Calculator", icon: tool11 },
+        { id: 4, name: "Asset Calculator", icon: tool12 },
+        { id: 5, name: "SIP Planner", icon: tool13 },
+        { id: 6, name: "Retirement Planning Calculator", icon: tool14 },
+        { id: 7, name: "Compounding Calculator", icon: tool15 },
+        { id: 8, name: "Lumpsum Calculator", icon: tool16 },
+    ];
+    return (
+        <div className="min-h-screen bg-[#f8f9f9]">
             {/* Breadcrumbs + Search */}
             <Header />
             <div className="grid grid-cols-6 items-center gap-4 px-12 py-6 ">
@@ -40,21 +61,120 @@ function FundNews() {
 
             </div>
             <div className="w-[full] h-[1px] bg-[#111111] mb-6 mx-12"></div>
-                <div className=" mx-auto px-4 sm:px-6 lg:px-8 py-6 grid grid-cols-1 lg:grid-cols-12 gap-6">
+            <div className=" mx-auto px-4 sm:px-6 lg:px-8 py-6 grid grid-cols-1 lg:grid-cols-12 gap-6">
                 {/* Left: Articles */}
 
-                <div className="lg:col-span-7 space-y-6">
+                <div className="lg:col-span-7 ">
                     <FundNewsPagination />
 
                 </div>
 
                 {/* Right: Sidebar */}
-                <div className=" lg:col-span-5 lg:mr-4"></div>
+                <div className=" lg:col-span-5 lg:mr-4">
+                    <div className="  overflow-hidden mb-6">
+                        <div className=" h-full flex items-center">
+                            <img
+                                src={aimImage}
+                                alt="Goals"
+                                className="w-full h-[200px] object-cover "
+                            />
+                        </div>
+                    </div>
+                    <div className="p-6">
+                        <h2 className="font-arial font-bold text-[16px] leading-[19px] tracking-[1px] text-gray-800 mb-4">
+                            Stories from Advisors
+                        </h2>
+                        <div className="w-full h-[1px] bg-[#111111] mb-4"></div>
+
+                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8  ">
+                            <div>
+                                <img
+                                    src={image3}
+                                    alt="Advisor"
+                                    className="w-full h-28 sm:h-32 object-cover  mb-2"
+                                />
+                                <p className="text-[#D99828] p-2 font-arial font-bold text-[10px] leading-[24px] tracking-[0.6px] uppercase py-1 inline-block">
+                                    FEATURED ADVISOR
+                                </p>
+                                <h4 className="font-merriweather p-2 font-bold text-[16px] leading-[28px] text-gray-800">
+                                    Active Funds are still a better choice in India...
+                                </h4>
+
+                                <p className="text-xs text-gray-500 p-2">
+                                    Rajiv Singh | Mumbai
+                                </p>
+                            </div>
+                            <div>
+                                <img
+                                    src={image4}
+                                    alt="Advisor"
+                                    className="w-full h-28 sm:h-32 object-cover mb-2"
+                                />
+                                <p className="text-[#D99828] p-2 font-arial font-bold text-[10px] leading-[24px] tracking-[0.6px] uppercase py-1 inline-block">
+                                    FEATURED ADVISOR
+                                </p>
+                                <h4 className="font-merriweather p-2 font-bold text-[16px] leading-[28px] text-gray-800">
+                                    Quant Fund can also be considered for core portfolio
+                                </h4>
+                                <p className="text-xs text-gray-500 p-2">
+                                    Supriya Raj | Mumbai
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center mt-2 mb-2">
+                        <h2
+                            className="font-[Arial] font-bold text-[16px] leading-[19px] tracking-[1px] 
+             text-[#000000] align-middle"
+                        >
+                            Tools and Calculators
+                        </h2>
+                        <button className="flex items-center font-[Arial] font-bold text-[11px] leading-[13px] tracking-[1.36px] uppercase text-[#000000] hover:text-blue-800 transition-colors">
+                            VIEW ALL <ArrowRight className="ml-1 h-4 w-4" />
+                        </button>
+                    </div>
+                    <div className="w-full h-[1px] bg-[#111111] mb-6"></div>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 max-w-3xl mx-auto">
+                        {features2.map((item) => (
+                            <div
+                                key={item.id}
+                                className="flex items-center gap-3 p-3 bg-white"
+                            >
+                                <img src={item.icon} alt={item.name} className="w-[24px] h-[24px]" />
+                                <span className="font-[Arial] font-normal text-[12px] leading-[150%] text-gray-700 align-middle">
+                                    {item.name}
+                                </span>
+
+                            </div>
+                        ))}
+                    </div>
+                    <div className="overflow-hidden mb-6 mt-6">
+                        <div className=" h-full flex items-center">
+                            <img
+                                src={aimImage}
+                                alt="Goals"
+                                className="w-full h-[130px] object-cover "
+                            />
+                        </div>
+                    </div>
+                     <div className="flex justify-between items-center mt-2 mb-2">
+                        <h2
+                            className="font-[Arial] font-bold text-[16px] leading-[19px] tracking-[1px] 
+             text-[#000000] align-middle"
+                        >
+                            Top Articles
+                        </h2>
+                        <button className="flex items-center font-[Arial] font-bold text-[11px] leading-[13px] tracking-[1.36px] uppercase text-[#000000] hover:text-blue-800 transition-colors">
+                            VIEW ALL <ArrowRight className="ml-1 h-4 w-4" />
+                        </button>
+                    </div>
+                    <div className="w-full h-[1px] bg-[#111111] mb-6"></div>
                 </div>
-        
-         <Footer/>
-    </div>
-  )
+            </div>
+
+            <Footer />
+        </div>
+    )
 }
 
 export default FundNews
--- src/Pages/Fund News/FundNewsPagination.jsx
+++ src/Pages/Fund News/FundNewsPagination.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import twoArrows from "../../assets/Images/Vector (1).png";
 
 function FundNewsPagination() {
     const categories = [
@@ -11,15 +12,24 @@ function FundNewsPagination() {
         "NFO",
         "Advisorkhoj Team",
     ];
+    const articles = Array.from({ length: 36 }, (_, i) => ({
+        id: i + 1,
+        title: "HDFC Mutual Fund launches HDFC Diversified Equity All Cap Active FOF",
+        description:
+            "To generate long-term capital appreciation / income by investing in units of domestic equity-oriented schemes based on varied market caps. There is no assurance that the investment objective of the Scheme will be achieved...",
+        author: "Eddie Lobanovskiy",
+        date: "09 Nov 2023",
+    }));
     const [active, setActive] = useState("All");
-     const [currentPage, setCurrentPage] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
+    const ITEMS_PER_PAGE = 12;
 
-  // Pagination logic
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const endIndex = startIndex + ITEMS_PER_PAGE;
-  const currentItems = articles.slice(startIndex, endIndex);
+    // Pagination logic
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+    const currentItems = articles.slice(startIndex, endIndex);
 
-  const totalPages = Math.ceil(articles.length / ITEMS_PER_PAGE);
+    const totalPages = Math.ceil(articles.length / ITEMS_PER_PAGE);
 
     return (
         <div>
@@ -29,8 +39,8 @@ function FundNewsPagination() {
             >
                 Most Popular Saving, Investing & Mutual Fund Articles
             </h2>
-            <div className="w-full h-[1px] bg-[#111111] mx-4"></div>
-            <div className="w-full mt-4 bg-white p-3 ms-4">
+            <div className=" h-[1px] bg-[#111111] mx-4"></div>
+            <div className=" mt-4 bg-white p-3 mx-4 mb-4">
                 <div className="flex justify-between items-center gap-2 sm:gap-3 overflow-x-auto lg:overflow-visible no-scrollbar">
                     {categories.map((category) => (
                         <button
@@ -38,7 +48,7 @@ function FundNewsPagination() {
                             onClick={() => setActive(category)}
                             className={`flex-shrink-0 px-3 py-1.5 text-[12px] font-[Arial] font-normal leading-[150%] rounded-sm border font-[Arial] font-normal text-[10px] leading-[120%] tracking-normal align-middle capitalize text-gray-700
           ${active === category
-                                    ? "bg-blue-600 text-white border-blue-600"
+                                    ? "bg-[#096FFA] text-white border-blue-600"
                                     : "bg-gray-100 text-gray-600 hover:bg-gray-200"
                                 } transition`}
                         >
@@ -46,6 +56,120 @@ function FundNewsPagination() {
                         </button>
                     ))}
                 </div>
+            </div>
+            <div className="grid grid-cols-1 mx-4 sm:grid-cols-2 gap-6">
+                {currentItems.map((item, index) => (
+                    <React.Fragment key={item.id}>
+                        {/* Insert banner after 6th card */}
+                        {index === 6 && (
+                            <div className="col-span-1 sm:col-span-2 w-full">
+                                <div className="bg-[linear-gradient(114.55deg,#ECD3BA_15.37%,#CAE5FF_75.11%)] mt-6 sm:mt-2 flex flex-col sm:flex-row items-center sm:items-start mb-2 p-4 sm:p-6 rounded-md">
+                                    <div className="flex-shrink-0 mb-4 sm:mb-0 sm:mr-6 w-full sm:w-[140px]">
+                                        <img
+                                            src={twoArrows}
+                                            alt="Double Up Arrow"
+                                            className="w-full h-auto object-contain"
+                                        />
+                                    </div>
+                                    <div className="flex-1 text-center sm:text-left mb-4 sm:mb-0">
+                                        <h3 className="font-serif font-bold text-[16px] pt-8 leading-[28px] text-gray-800 mb-1">
+                                            Portfolio Tracking Simplified
+                                        </h3>
+                                        <p className="font-sans text-[13px] leading-[18px] text-gray-700">
+                                            Track your wealth with the Ultimate Investment Portfolio Tracker
+                                        </p>
+                                    </div>
+                                    <div className="flex-shrink-0">
+                                        <button className="bg-[#096FFA] text-white px-4 sm:px-6 py-2 mt-10 sm:py-3 rounded-sm font-sans font-bold text-[11px] hover:bg-blue-700">
+                                            Track Your Portfolio
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+
+                        {/* Card */}
+                        <div className="bg-white border shadow-sm p-5 flex flex-col justify-between">
+                            <div>
+                                <h3 className="font-merriweather pr-24 font-bold text-[14px] leading-[150%] tracking-normal text-[#333333] mb-2">
+                                    {item.title}
+                                </h3>
+
+                                <p className="font-[Arial] font-normal text-[12px] leading-[150%] text-[#999999] mb-4 line-clamp-3">
+                                    {item.description}
+                                </p>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <p className="text-[12px] font-[Arial] font-bold text-gray-900">
+                                        by {item.author}
+                                    </p>
+                                    <p className="text-[12px] font-[Arial] text-gray-500 mt-2">
+                                        {item.date}
+                                    </p>
+                                </div>
+                                <button className="px-3 py-1.5 mt-2 bg-[#096FFA] text-white font-[Arial] font-bold text-[10px] leading-[12px] tracking-[1px] rounded hover:bg-blue-700 transition">
+                                    Read Now
+                                </button>
+
+                            </div>
+                        </div>
+                    </React.Fragment>
+                ))}
+            </div>
+            <div className="w-full h-[1px] bg-[#111111] mt-8 mx-4"></div>
+
+            {/* Pagination */}
+            <div className="flex justify-center items-center gap-2 mt-8 text-sm font-[Arial]">
+                {/* Prev */}
+                <button
+                    onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
+                    disabled={currentPage === 1}
+                    className="px-2 text-gray-500 hover:text-blue-600 disabled:text-gray-400 underline"
+                >
+                    Prev
+                </button>
+
+                {/* Page Numbers */}
+                <div className="flex items-center gap-2">
+                    <button
+                        onClick={() => setCurrentPage(Math.max(currentPage - 1, 1))}
+                        disabled={currentPage === 1}
+                        className="px-1 text-gray-600 hover:text-blue-600 disabled:text-gray-400"
+                    >
+                        &lt;
+                    </button>
+
+                    {Array.from({ length: totalPages }, (_, i) => (
+                        <button
+                            key={i + 1}
+                            onClick={() => setCurrentPage(i + 1)}
+                            className={`px-1 ${currentPage === i + 1
+                                ? "text-blue-600 font-bold underline"
+                                : "text-gray-600 hover:text-blue-600"
+                                }`}
+                        >
+                            {i + 1}
+                        </button>
+                    ))}
+
+                    <button
+                        onClick={() => setCurrentPage(Math.min(currentPage + 1, totalPages))}
+                        disabled={currentPage === totalPages}
+                        className="px-1 text-gray-600 hover:text-blue-600 disabled:text-gray-400"
+                    >
+                        &gt;
+                    </button>
+                </div>
+
+                {/* Next */}
+                <button
+                    onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))}
+                    disabled={currentPage === totalPages}
+                    className="px-2 text-gray-500 hover:text-blue-600 disabled:text-gray-400 underline"
+                >
+                    Next
+                </button>
             </div>
 
         </div>
